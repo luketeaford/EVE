62b24cb492356add5bd2c2b693f8d8a9237045cf
Luke Teaford <luke_teaford@abercrombie.com>
2015-05-16 14:19:24 -0400

    Missing file
    Not sure what happened, but this didn’t get added to the last commit.

--- js/eve.js
+++ js/eve.js
@@ -1,18 +1,3 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var EVE = {
-    synth: new AudioContext()
-};
-
-EVE.synth.start = function () {
-    'use strict';
-    var i;
-
-    for (i = 0; i < EVE.oscillators.length; i += 1) {
-        EVE.oscillators[i].start(0);
-    }
-};
-
 (function collapseModules() {
     'use strict';
     var moduleTitles = document.querySelectorAll('section > h2, .toggle'),
