a10c72cb279564310c33bba8d23d15e2853b7026
Luke Teaford <luke_teaford@abercrombie.com>
2015-07-13 08:22:08 -0400

    Keyboard control of octaveShift
    This is the first step to supporting keyboard control. Other keyboard
    events will need to work like this and include the likely scenario that
    a dataset won’t be available for every event.

--- TODO.md
+++ TODO.md
@@ -2,14 +2,10 @@
 
 ##Next Steps:
 * fix terrible ugliness
-
 * qwerty keyboard control
 
 
 ##TODO:
-
-* Rewrite calculatePitch and setPitch
-
 * MIDI
 
 * Harmonic Osc needs modulation amounts moved into each channel
--- js/eve.js
+++ js/eve.js
@@ -32,10 +32,10 @@ EVE.keyboard = {
     debug: true,
     octaveShift: 0,
     scope: document.getElementById('keyboard'),
-    shiftOctave: function () {
+    shiftOctave: function (direction) {
         'use strict';
         var oct = EVE.keyboard.octaveShift,
-            shift = this.dataset.shift;
+            shift = this.dataset ? this.dataset.shift : direction;
 
         if ((oct > -2 && shift < 0) || (oct < 2 && shift > 0)) {
             EVE.keyboard.octaveShift = oct + parseFloat(shift);
@@ -46,10 +46,20 @@ EVE.keyboard = {
         }
 
     },
-    test: function (e) {
+    bus: function (e) {
         'use strict';
-        if (console) {
+        if (EVE.keyboard.debug && console) {
             console.log(e.which);
+        }
+        switch (e.which) {
+        case 45:// -
+        case 95:// _
+            EVE.keyboard.shiftOctave(-1);
+            break;
+        case 61:// =
+        case 43:// +
+            EVE.keyboard.shiftOctave(1);
+            break;
         }
     },
     touch: function (e) {
@@ -65,7 +75,7 @@ EVE.keyboard = {
     var buttons = document.getElementsByClassName('octave-shift'),
         i;
     for (i = 0; i < buttons.length; i += 1) {
-        buttons[i].addEventListener('click', EVE.keyboard.shiftOctave);
+        buttons[i].addEventListener('click', EVE.keyboard.bus);
     }
     document.addEventListener('keypress', EVE.keyboard.test);
 }());
