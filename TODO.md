3e39d080ab7443d98f3c8b1381f4f868fbcb1da0
Luke Teaford <luke_teaford@abercrombie.com>
2015-07-17 08:28:04 -0400

    Work in progress (SVG)
    I’m testing the viability of SVG knobs. This is a simple demo of knob
    rotation working, but obviously it needs to be linked to the value of
    the parameter, capped to minimum and maximum values (-135deg for full
    CCW rotation), and must resume from current position when grabbed.

--- TODO.md
+++ TODO.md
@@ -2,6 +2,10 @@
 
 ##Next Steps:
 * fix terrible ugliness
+  * The focus state of the push button is better
+  * Will need a new focus state
+  * size keyboard keys appropriately
+  * pad performance controls
 * qwerty keyboard control
 
 
--- js/eve.js
+++ js/eve.js
@@ -526,6 +526,70 @@ EVE.slider = {
 
 }());
 
+// SVG knobs
+EVE.knob = {
+    currentKnob: null,
+    debug: false,
+    grab: function (e) {
+        'use strict';
+        EVE.knob.grab.origin = {
+            x: e.pageX,//formerly screenX
+            y: e.pageY//formerly screenY
+        };
+        EVE.knob.currentKnob = this;
+        document.addEventListener('mousemove', EVE.knob.twist);
+        document.addEventListener('touchmove', EVE.knob.twist);
+    },
+    rotate: function () {
+        'use strict';
+        var x = null;
+        EVE.knob.currentKnob.style.webkitTransform = x;
+        EVE.knob.currentKnob.style.transform = x;
+    },
+    twist: function (e) {
+        'use strict';
+        // TODO e.x is nicer, but not available on iOS
+        var deg = e.pageX - EVE.knob.grab.origin.x,
+            rotate = 'rotate(' + deg + 'deg)';
+
+        if (EVE.knob.debug && console) {
+            console.log('Difference x', e.pageX - EVE.knob.grab.origin.x);
+            console.log('Difference y', e.pageY - EVE.knob.grab.origin.y);
+        }
+
+        if (EVE.knob.currentKnob.style.webkitTransform) {
+            // iOS
+            EVE.knob.currentKnob.style.webkitTransform = rotate;
+        } else if (EVE.knob.currentKnob.style.mozTransform) {
+            // Firefox
+            EVE.knob.currentKnob.style.mozTransform = rotate;
+        } else {
+            EVE.knob.currentKnob.style.transform = rotate;
+        }
+
+        document.addEventListener('mouseup', EVE.knob.release);
+        document.addEventListener('touchend', EVE.knob.release);
+    },
+    release: function () {
+        'use strict';
+        console.log('Knob released');
+        document.removeEventListener('mousemove', EVE.knob.twist);
+        document.removeEventListener('mouseup', EVE.knob.release);
+        document.removeEventListener('touchmove', EVE.knob.twist);
+        document.removeEventListener('touchend', EVE.knob.release);
+    }
+};
+
+(function bindKnobs() {
+    'use strict';
+    var knobs = document.getElementsByClassName('knob'),
+        i;
+    for (i = 0; i < knobs.length; i += 1) {
+        knobs[i].addEventListener('mousedown', EVE.knob.grab);
+        knobs[i].addEventListener('touchstart', EVE.knob.grab);
+    }
+}());
+
 EVE.button = {
     debug: true,
     press: function () {
