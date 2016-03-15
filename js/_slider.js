1b5d84f8f5e15ac4dc728d299610b32dc0099b3c
Luke Teaford <luketeaford@gmail.com>
2016-03-14 23:23:32 -0400

    Work in progress knobs
    These are centered a little better

--- js/_slider.js
+++ js/_slider.js
@@ -8,8 +8,7 @@ EVE = (function (module) {
         'performance': 'updateperformance',
         'timbre-env': 'updatetimbreenv',
         'vca': 'updatevca'
-    },
-        oscInput = document.querySelector('input[type=range]');
+    };
 
     module.slider = {
         grab: function (event) {
@@ -19,27 +18,19 @@ EVE = (function (module) {
 
             if (event.target.type === 'range') {
                 program = event.target.dataset.program;
-                update = updateMethods[event.target.parentElement.parentElement.id];
+                update = updateMethods[event.target.parentElement.parentElement.parentElement.id];
                 x = event.target.dataset.curve === 'lin' ? 1 : event.target.value;
                 module.preset[program] = event.target.value * x;
-                // EXPERIMENT
                 event.target.nextElementSibling.style.transform = 'rotate(' + (-135 + (x * 270)) + 'deg)';
 
                 return event.target.dispatchEvent(module.events[update]);
             }
 
             return;
-        },
-
-        test: function (event) {
-            event.target.parentElement.style.backgroundColor = '#ff0';
-            return;
         }
     };
 
     document.addEventListener('input', module.slider.grab);
-    oscInput.addEventListener('mousedown', module.slider.test);
-    oscInput.addEventListener('touchstart', module.slider.test);
 
     return module;
 }(EVE));
