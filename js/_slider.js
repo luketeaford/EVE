19a2fccf3ed0b3473903d13cce967419c0952fea
Luke Teaford <luketeaford@gmail.com>
2016-03-14 20:31:47 -0400

    Rough draft of working knobs
    Webkit only right now and will need to make changes to slider
    javascript to work on the correct parameters

--- js/_slider.js
+++ js/_slider.js
@@ -8,7 +8,8 @@ EVE = (function (module) {
         'performance': 'updateperformance',
         'timbre-env': 'updatetimbreenv',
         'vca': 'updatevca'
-    };
+    },
+        oscInput = document.querySelector('input[type=range]');
 
     module.slider = {
         grab: function (event) {
@@ -21,14 +22,24 @@ EVE = (function (module) {
                 update = updateMethods[event.target.parentElement.parentElement.id];
                 x = event.target.dataset.curve === 'lin' ? 1 : event.target.value;
                 module.preset[program] = event.target.value * x;
+                // EXPERIMENT
+                event.target.nextElementSibling.style.transform = 'rotate(' + (-135 + (x * 270)) + 'deg)';
+
                 return event.target.dispatchEvent(module.events[update]);
             }
 
+            return;
+        },
+
+        test: function (event) {
+            event.target.parentElement.style.backgroundColor = '#ff0';
             return;
         }
     };
 
     document.addEventListener('input', module.slider.grab);
+    oscInput.addEventListener('mousedown', module.slider.test);
+    oscInput.addEventListener('touchstart', module.slider.test);
 
     return module;
 }(EVE));
